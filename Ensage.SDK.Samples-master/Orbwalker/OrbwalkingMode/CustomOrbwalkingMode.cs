44eca7973dd31aa4e51a7c57e5e521cbab331dab
limurk <44515197+limurk@users.noreply.github.com>
2019-12-01 01:19:04 +0200

    Update CustomOrbwalkingMode.cs

--- Ensage.SDK.Samples-master/Orbwalker/OrbwalkingMode/CustomOrbwalkingMode.cs
+++ Ensage.SDK.Samples-master/Orbwalker/OrbwalkingMode/CustomOrbwalkingMode.cs
@@ -12,7 +12,7 @@ namespace OrbwalkingMode
 
     using Ensage;
     using Ensage.SDK.Abilities;
-    using Ensage.SDK.Abilities.npc_dota_hero_zuus;
+    using Ensage.SDK.Abilities.npc_dota_hero_sky;
     using Ensage.SDK.Extensions;
     using Ensage.SDK.Helpers;
     using Ensage.SDK.Orbwalker.Metadata;
@@ -32,7 +32,7 @@ namespace OrbwalkingMode
 
         private readonly Hero hero;
 
-        private zuus_arc_lightning arcLightning;
+        private skywrath_mage_arcane_bolt arcanebolt;
 
         private Settings settings;
 
@@ -61,18 +61,18 @@ namespace OrbwalkingMode
 
         public override async Task ExecuteAsync(CancellationToken token)
         {
-            if (this.arcLightning.CanBeCasted)
+            if (this.arcanebolt.CanBeCasted)
             {
                 var creep = EntityManager<Creep>.Entities
-                    .Where(x => x.IsValid && x.IsAlive && x.IsSpawned && x.IsEnemy(this.Owner) && this.arcLightning.CanHit(x))
+                    .Where(x => x.IsValid && x.IsAlive && x.IsSpawned && x.IsEnemy(this.Owner) && this.arcanebolt.CanHit(x))
                     .OrderBy(x => x.Health)
-                    .FirstOrDefault(x => this.arcLightning.GetDamage(x) >= x.Health);
+                    .FirstOrDefault(x => this.arcanebolt.GetDamage(x) >= x.Health);
 
                 if (creep != null)
                 {
                     Log.Warn("Killing " + Game.Localize(creep.Name) + " with " + creep.Health + " health");
-                    this.arcLightning.UseAbility(creep);
-                    await Task.Delay(this.arcLightning.GetCastDelay(creep), token);
+                    this.arcanebolt.UseAbility(creep);
+                    await Task.Delay(this.arcanebolt.GetCastDelay(creep), token);
                 }
             }
 
@@ -83,14 +83,14 @@ namespace OrbwalkingMode
 
         protected override void OnActivate()
         {
-            if (this.hero?.HeroId != HeroId.npc_dota_hero_zuus)
+            if (this.hero?.HeroId != HeroId.npc_dota_hero_skywrath_mage)
             {
                 // only for zeus
                 return;
             }
 
             this.settings = new Settings(this.Orbwalker.Settings.Factory.Parent);
-            this.arcLightning = this.abilityFactory.GetAbility<zuus_arc_lightning>();
+            this.arcanebolt = this.abilityFactory.GetAbility<skywrath_mage_arcane_bolt>();
         }
 
         protected override void OnDeactivate()
